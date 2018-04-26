789136e1bef736b5173bb190790ca5b4e2d85d66
Yoni Carmel <yonica7@gmail.com>
2018-04-26 18:52:16 +0300

    make it better

--- client/app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/MainActivity.java
+++ client/app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.avjindersinghsekhon.minimaltodo.Main;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,14 @@ import com.example.avjindersinghsekhon.minimaltodo.AppDefault.AppDefaultActivity
 import com.example.avjindersinghsekhon.minimaltodo.R;
 import com.example.avjindersinghsekhon.minimaltodo.Settings.SettingsActivity;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppDefaultActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,36 @@ public class MainActivity extends AppDefaultActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(false);
         }
+
+        String result = null;
+        try {
+            URL url = new URL("http://www.mocky.io/v2/5ae1efc72d00005e009d7f7f");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            result = inputStreamToString(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String inputStreamToString(InputStream is) {
+        String rLine = "";
+        StringBuilder answer = new StringBuilder();
+
+        InputStreamReader isr = new InputStreamReader(is);
+
+        BufferedReader rd = new BufferedReader(isr);
+
+        try {
+            while ((rLine = rd.readLine()) != null) {
+                answer.append(rLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return answer.toString();
     }
 
     @Override
